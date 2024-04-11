463fefe57abb87ff32c03b640f5b6d56ee1df10d
Kaveri Gojare <kaverigojre@gmail.com>
2024-04-11 19:03:22 +0530

    added

--- git.py
+++ git.py
@@ -6,8 +6,9 @@ for i in range(20):
     with open('test.txt','a') as file:
         file.write(d+'\n')
     os.system('git add test.txt')
-    os.system('git commit --date=" 2020-'+str(rand)+'-'+d+'" -m 1')
+    os.system('git commit --date=" 2021-'+str(rand)+'-'+d+'" -m 1')
 os.system('git push -u origin main')
+
 
 #git commit --amend --no-edit --date="Fri Nov 6 20:00:00 2015 -0600" 
 #git fetch origin master
